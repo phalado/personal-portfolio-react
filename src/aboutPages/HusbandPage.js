19e5dd53d705db912be28869723c4c017a325333
phalado <phalado@gmail.com>
2020-06-01 17:26:41 -0300

    Minor correction

--- src/aboutPages/HusbandPage.js
+++ src/aboutPages/HusbandPage.js
@@ -5,10 +5,10 @@ const HusbandPage = () => (
     <h1>Together since July 20, 2008</h1>
     <h1>Married since October 10, 2015</h1>
     <div>
-      <img src="../contents/about/marry1.jpg" alt="Married1" style={{ margin: '2rem' }} />
-      <img src="../contents/about/marry2.jpg" alt="Married2" style={{ margin: '2rem', width: '45%' }} />
-      <img src="../contents/about/marry3.jpg" alt="Married3" style={{ margin: '2rem', width: '45%' }} />
-      <img src="../contents/about/marry4.jpg" alt="Married4" style={{ margin: '2rem', width: '90%' }} />
+      <img src="../contents/about/marry1.jpg" alt="Married1" style={{ margin: '1rem', width: '90%' }} />
+      <img src="../contents/about/marry2.jpg" alt="Married2" style={{ margin: '1rem', width: '45%' }} />
+      <img src="../contents/about/marry3.jpg" alt="Married3" style={{ margin: '1rem', width: '45%' }} />
+      <img src="../contents/about/marry4.jpg" alt="Married4" style={{ margin: '1rem', width: '90%' }} />
     </div>
   </div>
 );
